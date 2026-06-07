f30b596a576e96e938ff893ec6ff2a9149b535c4
Techabilla <andythebritton@gmail.com>
2026-06-07 17:20:55 +0930

    update api url

--- js/bugsonglistapi.js
+++ js/bugsonglistapi.js
@@ -2,16 +2,15 @@
 // var apiURL = "https://script.google.com/macros/s/AKfycbx-0s1grPv0Wj_wXZUDRggB7Eac_c4TGHkMQ1aNOcNv41eCeg/exec"
                 
 // V30
-var apiURL = "https://script.google.com/macros/s/AKfycbzjh_JtaR2mWAer_QJFIbfozztxOpXMwPizbvTgeFvwlaCz1m1iTkc/exec"
+// var apiURL = "https://script.google.com/macros/s/AKfycbzjh_JtaR2mWAer_QJFIbfozztxOpXMwPizbvTgeFvwlaCz1m1iTkc/exec"
 
+// V35 - enable v8
+var apiURL = "https://script.google.com/macros/s/AKfycbxGZYkrWtCEjsUpgJJV9sj8gzCJo6QyEEZtkHpgj0fe-Ow4hVpqccV52nWLzyGItyA/exec"
 
 // HEAD
 // var accessToken = "ya29.a0AfH6SMAwrKWqNEGwxbq4cV6xNoAeGNFFgUrqJio6jhihpd1fXSTpDdGyKEMh4_YfuuEXGpQS7abUTkcQx9TaX8MEfZOo3hyPCMG25p_NWwlakjTV3h5rP7f2c8849E_Mzfd5_vTbomNdG2epPBKyKdGLJptLav8DxFNtjA"
 // var apiURL = "https://script.google.com/macros/s/AKfycbwU0YwFnwGdrGJlNKnwUsP8qCE8r7zGujtbDa66HA/dev?access_token=" + accessToken + "&nocache=1"
 
-// access token
-// if this stops work
-// ya29.a0AfH6SMCruOmck7XX5yJVAcMXi_GOnMgjVOtTNzzetiyURCUl3UC9CbnWLuq-X_qHWIBDLpEl0-rBmoBDiNhqNDzYpXSUjKzUl7bvqFtSxTJDwAhUPzRfFrjrk7Gxp-2pQISEMh3Nu6PUtBHz43tXoU0g_TJ26nUC2t6Xjg
 
 // how to use google-apps-script dev mode with access token:
 // https://github.com/tanaikech/taking-advantage-of-Web-Apps-with-google-apps-script#howtousedevmode
